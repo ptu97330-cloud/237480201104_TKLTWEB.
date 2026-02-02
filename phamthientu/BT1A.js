a40b88d9fbbe3e76054abf0930b4918be6316ed4
ptu97330-cloud <ptu97330@gmail.com>
2026-02-02 16:40:02 +0700

    Add files via upload

---
+++ phamthientu/BT1A.js
@@ -0,0 +1,9 @@
+function TenvaTuoi(){
+    var Ten,Tuoi;
+    Ten = prompt("Bạn hãy nhập vào tên","");
+    Tuoi = prompt("Bạn hãy nhập vào tuổi:",20);
+    document.write("Chào bạn: <B>" + Ten + "</B>");
+    document.write("<BR>");
+    document.write("Tuổi của bạn là: <U>" +Tuoi + "</U>");
+}
+TenvaTuoi();
---
+++ phamthientu/BT1B.js
@@ -0,0 +1,8 @@
+function HoTenSVvaLop(){
+    var HoTenvaLop;
+    HoTen = prompt("Bạn hãy nhập họ tên:","");
+    Lop = prompt("Bạn hãy nhập tên lớp:","");
+    document.write("<p class ='a'> Lớp:" + Lop + "</p>"  );
+    document.write("<p class='b'>Họ Tên:" + HoTen + "</p>");
+}
+HoTenSVvaLop();
